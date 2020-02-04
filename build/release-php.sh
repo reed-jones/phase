82a49a1e34f033221715cac7cc73c51b989feb2a
Reed Jones <reed.jones@pderas.com>
2020-02-03 22:50:16 -0400

    wip

---
+++ build/release-php.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+VERSION=$1
+VERSION_REGEX="^v[0-9]+\.[0-9]+\.[0-9]+$"
+
+if [[ $VERSION =~ $VERSION_REGEX ]]
+then
+    echo "Version must match the format vX.X.X"
+
+    exit 1
+fi
+
+git tag $VERSION
+git push origin --tags
+
+for REMOTE in routing state
+do
+    echo ""
+    echo ""
+    echo "Releasing $REMOTE";
+
+    TMP_DIR="/tmp/phased-php"
+    REMOTE_URL="git@github.com:reed-jones/phase-$REMOTE-php.git"
+
+    rm -rf $TMP_DIR;
+    mkdir $TMP_DIR;
+
+    (
+        cd $TMP_DIR;
+
+        git clone $REMOTE_URL .
+        git checkout master
+        git tag $VERSION
+        git push origin --tags
+    )
+done
